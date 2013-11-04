721ef2c3517a2d9106d9b03991cd2fb82d8d4e06
maginemu <lonely.two.mn+github@gmail.com>
2013-11-05 02:01:38 +0900

    Added index of test

---
+++ test/test.index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var chai = require('chai');
+should = chai.should();
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+
+//var webpay = require('webpay');
+
+describe('webpay.account', function() {
+
+	describe('.retrieve', function() {
+
+		before(function() {
+			// setup
+		});
+
+		it('should returns correct object');
+		/*
+		  , function() {
+			webpay.account.retrieve(function(err, res) {
+				// response object should be correct
+			});
+		});
+		*/
+	});
+
+	describe('.deleteData', function() {
+		it('should return true');
+		/*
+		  , function() {
+		  webpay.account.deleteData(function(err, res) {
+		  // with no err, res should be true
+		  });
+		  });
+		  */
+	});
+});
