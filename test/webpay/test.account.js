0051dc20945598a675cc5bd9a0e230357f142639
maginemu <lonely.two.mn+github@gmail.com>
2014-01-20 03:50:41 +0900

    Added `account/delete/data` and improved tests

--- test/webpay/test.account.js
+++ test/webpay/test.account.js
@@ -8,34 +8,54 @@ var helper = require('../helper');
 var webpay = require('../../');
 
 describe('webpay.account', function() {
+	var server = null;
+	before(function() {
+		webpay.api_base = 'http://localhost:2121';
+		var map = helper.mapResponseForExpress();
+		var mock = express();
+
+		var response_retrieve = map['account/retrieve'];
+		mock.get('/v1/account/retrieve', function(req, res) {
+			var r = response_retrieve;
+			delete r.header['Connection'];
+			res.set(r.header);
+			res.send(r.status, r.body);
+		});
+
+		var response_delete = map['account/delete'];
+		mock.del('/v1/account/delete/data', function(req, res) {
+			var r = response_delete;
+			delete r.header['Connection'];
+			res.set(r.header);
+			res.send(r.status, r.body);
+		});
+
+		server = http.createServer(mock).listen(2121);
+	});
+
+	after(function() {
+		server.close();
+	});
+
+
 	describe('.retrieve', function() {
-		var server = null;
-		before(function() {
-			webpay.api_base = 'http://localhost:2121';
-			var map = helper.mapResponseForExpress();
-			var mock = express();
-
-			var response = map['account/retrieve'];
-			mock.get('/v1/account/retrieve', function(req, res) {
-				delete response.header['Connection'];
-				res.set(response.header);
-				console.log('res.set:', response.header);
-
-				res.send(response.status, response.body);
-
-				console.log('res.send:', response.status, response.body);
-
-			});
-			server = http.createServer(mock).listen(2121);
-		});
-
-		after(function() {
-			server.close();
-		});
-
 		it('expected to retrieve account object', function(done) {
 			webpay.client.account().retrieve(function(err, res) {
-				console.log(err, res);
+				expect(err).to.be.null;
+				expect(res).to.have.property('object').and.eql('account');
+				expect(res).to.have.property('id').and.eql('acct_2Cmdexb7J2r78rz');
+				expect(res).to.have.property('email').and.eql('test-me@example.com');
+				expect(res).to.have.property('currencies_supported').and.eql(['jpy']);
+				done();
+			});
+		});
+	});
+
+	describe('.delete', function() {
+		it('expected to return true', function(done) {
+			webpay.client.account().delete(function(err, res) {
+				expect(err).to.be.null;
+				expect(res).to.be.true;
 				done();
 			});
 		});
